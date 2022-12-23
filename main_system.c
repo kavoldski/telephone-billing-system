b8256c6f57ce4658e31f5783b4a2392705ac5f1a
kavoldski <kavoldski@gmail.com>
2022-12-23 09:58:18 +0800

    Create main_system.c

---
+++ main_system.c
@@ -0,0 +1,9 @@
+// Project : Telephone Billing System
+// Author : Cedric, Poji, Jessie, Chang, Frank
+// Matrix No : 86350, , , 86356, 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
